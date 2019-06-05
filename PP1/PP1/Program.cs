7817fbefcb52afd70802cdb456bdc99f1a1edc2a
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-06-05 15:39:31 +0200

    Add files via upload

---
+++ PP1/PP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? " );
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+                Console.WriteLine("Reversed name: " + reversed);
+        }
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+            return new string(array);
+        }
+    }
+}
---
+++ SEXE4/SEXE4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace SEXE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few words: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
+    }
+}
